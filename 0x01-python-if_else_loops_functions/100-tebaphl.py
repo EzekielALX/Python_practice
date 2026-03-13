bbe21b92a63b0ece9f70f25365331163430f6b0d
Ezekiel <Kehindeeademola731@gmail.com>
2026-03-13 09:35:56 +0100

    Another practice completed

---
+++ 0x01-python-if_else_loops_functions/100-tebaphl.py
@@ -0,0 +1,2 @@
+for i in range(26):
+    print(f'{chr(122 - i)}{chr(89 - i)}', end='')
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 def uppercase(str):
     result = ""
-    for ord(i) in str:
+    for (i) in str:
         if ord(i) >= ord('a') and ord(i) <= ord('z'):
             result += chr(ord(i) - 32)
         else:
             result += ord(i)
     print(result)
 
-uppercase("The quick fox jump over ")
+uppercase("gi")
